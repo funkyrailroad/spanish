499e414d83b69738f231e0097673750450e59d5e
Jake Atwell <funkyrailroad@Jake-Atwells-MacBook-Pro.local>
2018-01-07 23:15:38 +0100

    Changed how each line is written
    Characters are written to a string first which allows the final tab to
    be replace with a newline.

--- convert-to-anki.py
+++ convert-to-anki.py
@@ -5,20 +5,24 @@ filename = sys.argv[1]
 readfile = open(filename, 'r')
 
 newname = filename.split('.')[0] + ".ank"
-writefile = open(newname, 'w')
+writefile = open(newname, 'w+')
+line_list = ''
 
 for line in readfile.readlines():
     # skip comment lines
     if line[0] == '#':
         continue
-
     # line breaks
     if line == "\n":
-        writefile.write("\n")
+        line_list = line_list[:-1] + '\n'
+        print repr(line_list)
+        writefile.write(line_list)
+        line_list = ''
+        #exit()
         continue
     line = line.split("\n")[0]
-    writefile.write(line)
-    writefile.write('\t')
+    line_list += line
+    line_list += '\t'
 
 writefile.write('\n')
 
